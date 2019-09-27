66e7e86260d3ce4b5b728b19b6e5d8c47c150e37
fazt <fazt@faztweb.com>
2019-09-27 09:42:15 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.use(require('./routes/index'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000);
+console.log('Server on port 3000')
---
+++ src/routes/index.js
@@ -0,0 +1,50 @@
+const { Router } = require('express');
+const router = Router();
+
+const nodemailer = require('nodemailer');
+
+router.post('/send-email', async (req, res) => {
+    const { name, email, phone, message } = req.body;
+
+    contentHTML = `
+        <h1>User Information</h1>
+        <ul>
+            <li>Username: ${name}</li>
+            <li>User Email: ${email}</li>
+            <li>PhoneNumber: ${phone}</li>
+        </ul>
+        <p>${message}</p>
+    `;
+
+    let transporter = nodemailer.createTransport({
+        host: 'mail.fazttech.net',
+        port: 587,
+        secure: false,
+        auth: {
+            user: 'testtwo@fazttech.net',
+            pass: 'testtwocontraseña'
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    let info = await transporter.sendMail({
+        from: '"FaztTech Server" <testtwo@fazttech.xyz>', // sender address,
+        to: 'fazttech@gmail.com',
+        subject: 'Website Contact Form',
+        // text: 'Hello World'
+        html: contentHTML
+    })
+
+    console.log('Message sent: %s', info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+    res.redirect('/success.html');
+});
+
+module.exports = router;
